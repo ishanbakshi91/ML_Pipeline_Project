d3391ae81dff4d3c90503b7e17dfe2bc6aa31364
Ishan <ishanbakshi91@gmail.com>
2024-01-10 01:08:08 +0530

    Project Structure

---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "ML_Pipeline_Project"
+
+list_of_files = [
+    #".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_monitoring.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/training_pipeline.py",
+    f"src/{project_name}/pipeline/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "main.py",
+    "app.py",
+    "logs.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+       # logging.INFO(f"Creating directory:{filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+           # logging.INFO(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"File is already present at: {filepath}")
+
+
